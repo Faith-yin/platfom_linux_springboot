c0bb4773233fb4ebf3623b8e2f2cf57249597092
yinpengfei <1207828715@qq.com>
2020-03-19 12:09:02 +0800

    admin增加最后登录时间字段

--- src/main/java/com/ypf/entity/Admin.java
+++ src/main/java/com/ypf/entity/Admin.java
@@ -1,8 +1,11 @@
 package com.ypf.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
 
 /**
  * 管理员信息
@@ -16,5 +19,7 @@ public class Admin {
     private String username; //名称
     private String password; //密码
     private String photo; //头像
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",  timezone="GMT+8")
+    private Timestamp lastLoginTime; //最后登录时间
 
 }
